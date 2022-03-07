cc632dcc98be6f228798b7b6b2eca968bae1959e
tanakamaikeru <tanakamaikeru@users.noreply.github.com>
2022-03-07 16:55:03 +0000

    middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { ControllerModule } from './controllers/controller.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { OrmModule } from './models/orm.module';
 
 @Module({
   imports: [ControllerModule, OrmModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware)
+      .forRoutes({ path: '(.*)', method: RequestMethod.ALL });
+  }
+}
